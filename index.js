f264da3f1fab06728be2a1a4e995631ad0cadc52
introtoxicXXl <minhajulabedin648@gmail.com>
2023-12-02 14:32:14 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -6,6 +8,36 @@ app = express()
 app.use(express.json())
 app.use(cors())
 
+// minhajulabedin648
+// XnzXe1uXxhiFkZ1h
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.v4gjlif.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+
+
+
+        
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Rivo is running')
